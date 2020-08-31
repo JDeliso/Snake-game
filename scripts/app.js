9d868dbc4b0e0b3a5dadcc26daf44ed2affc439f
jdeliso <J_Deliso@yahoo.com>
2020-08-31 13:02:56 -0400

    added generateSnake function

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,27 @@
 console.log("Hello World");
 
 const $gameBoard = $(".game-board");
+const gameSize = 20;
+const snakeStart = Math.floor((gameSize * gameSize) / 2)+(gameSize/2);
+const snake = [snakeStart, snakeStart + 20, snakeStart + 40];
 
+
+// generates the snake each tick
+function generateSnake() {
+    let $cell = $('.cell');
+    console.log($cell);
+    for (let i = 0; i < snake.length; i++) {
+        for (let j in $cell) {
+            if (j == snake[i]) {
+                const $snakeCell = $cell[j];
+                $snakeCell.classList.add("snake-cell");
+                console.log($snakeCell);
+            }
+        }
+    }
+}
+
+// generates board on tick start
 function generateBoard(size) {
     $gameBoard.css('--grid-size', size);
     
@@ -12,4 +32,12 @@ function generateBoard(size) {
   };
 };
 
-generateBoard(20);
+function startGame() {
+    $(".start-button").remove();
+    generateBoard(gameSize);
+    generateSnake();
+}
+
+
+// Listens for click on the play button, on click removes menu items and generates board
+$(".start-button").on("click", startGame);
