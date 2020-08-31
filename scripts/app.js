91e1419fc8cb8c592e30c5caed412ea3925ea348
jdeliso <J_Deliso@yahoo.com>
2020-08-31 12:06:18 -0400

    generateBoard added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+const $gameBoard = $(".game-board");
+
+function generateBoard(size) {
+    $gameBoard.css('--grid-size', size);
+    
+    for (let i = 0; i < (size * size); i++) {
+        const $cell = $("<div class='cell'></div>");
+        $cell.textext = (i + 1);
+        $gameBoard.append($cell);
+  };
+};
+
+generateBoard(20);
